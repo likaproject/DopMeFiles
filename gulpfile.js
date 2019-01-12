7d00fb4c64f7e0d6d253a0786773855408b8cc57
Kaidalova Elena <lika_project@ukr.net>
2019-01-12 19:57:30 +0200

    gulp-tasks for production

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,26 @@
-const gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    nunjucks = require('gulp-nunjucks-render'),
-    watch = require('gulp-watch');
-	
-gulp.task('default', ['sass', 'html']);
+const   gulp = require('gulp'),
+        sass = require('gulp-sass'),
+        nunjucks = require('gulp-nunjucks-render'),
+        watch = require('gulp-watch'),
+	  	autoprefixer = require('gulp-autoprefixer'),
+	  	minCss = require('gulp-clean-css'),
+	  	rename = require('gulp-rename'),
+	  	minJs = require('gulp-uglify'),
+	  	clean = require('gulp-clean');
+
+
+//--------------------------------
+gulp.task('clean', () => {
+	return gulp.src('dist', {read: false})
+			.pipe(clean());
+});
+//---------------------------------
+gulp.task('default', function() {
+	gulp.watch('src/**/*.html', ['html']);
+    gulp.watch('src/scss/*.scss', ['sass']);
+	gulp.watch('dist/*.css', ['min']);
+    gulp.watch('dist/main.min.css', ['autoprefixer']);
+});
 //--------------------------------
 const html = gulp.task('html', () =>
     gulp.src("./src/index.html")
@@ -12,11 +29,57 @@ const html = gulp.task('html', () =>
 );
 //--------------------------------
 gulp.task('sass', function () {
-    return gulp.src('src/scss/main.scss')
+    return gulp.src('./src/scss/main.scss')
         .pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer({
+				browsers: ['last 2 versions'],
+				cascade: false
+			}))
+		.pipe(minCss({compatibility: 'ie8'}))
+		.pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('dist'));
+	
 });
-//--------------------------------
-gulp.task('watch:css', function() {
-    gulp.watch('src/scss/*.scss', ['sass']);
+//------------------------------------
+// useref!!!
+//-------------------------------------
+gulp.task('minJs', () => {
+	return gulp.src('src/js/*.js')
+			.pipe(minJs())
+			.pipe(gulp.dest('dist/js'));
 });
+//-----------------------------------------
+gulp.task('build', ['clean', 'html', 'sass', 'minJs']);
+//------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
